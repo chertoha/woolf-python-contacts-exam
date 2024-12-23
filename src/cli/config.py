2007a9bbb48e2eff370b12b1201c22c449a8749a
Anton <a.chertok@akatech.com.ua>
2024-12-23 10:19:20 +0000

    feat autocompleter

--- src/cli/config.py
+++ src/cli/config.py
@@ -5,4 +5,13 @@ class Commands(Enum):
     ADD = "add"
     FIND = "find"
     UPDATE = "update"
+
     BIRTHDAY = "birthday"
+    ADD_BIRTHDAY = "add-birthday"
+
+    ADDRESS = "address"
+    ADD_ADDRESS = "add-address"
+
+    EMAIL = "email"
+    ADD_EMAIL = "add-email"
+    UPDATE_EMAIL = "update-email"
--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
+import readline
 from cli.commands import execute_command
+from cli.config import Commands
 from cli.parser import parse_input
 from services.command_handle_service import CommandHandleService
 
 
+def completer(text, state):
+    commands = [command.value for command in Commands]
+    matches = [cmd for cmd in commands if cmd.startswith(text)]
+    if state < len(matches):
+        return matches[state]
+    return None
+
+
 def main() -> None:
+
+    readline.set_completer(completer)  # type: ignore
+    readline.parse_and_bind("tab: complete")  # type: ignore
 
     while True:
         user_input = input("Enter a command: ").strip()
@@ -18,3 +31,37 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+
+# from prompt_toolkit import PromptSession
+# from prompt_toolkit.completion import WordCompleter
+# from prompt_toolkit.styles import Style
+
+# # Команды для автокомплита
+# COMMANDS = ["add-phone", "phone", "address", "specification"]
+
+# # Настраиваем стили
+# custom_style = Style.from_dict({
+#     'completion-menu.completion': 'bg:#444444 fg:#ffffff',  # Цвет подсказок
+#     'completion-menu.completion.current': 'bg:#888888 fg:#ffffff underline',  # Выбранный пункт
+#     'completion-menu.meta': 'bg:#444444 fg:#aaaaaa',  # Полупрозрачные мета-информации
+# })
+
+# # Создаём автокомплитер
+# completer = WordCompleter(COMMANDS, ignore_case=True, meta_dict={
+#                           cmd: "Command" for cmd in COMMANDS})
+
+# # Создаём сессию
+# session = PromptSession(completer=completer, style=custom_style)
+
+# # Основной цикл
+# while True:
+#     try:
+#         user_input = session.prompt("Enter a command: ")
+#         if user_input == "exit":
+#             print("Goodbye!")
+#             break
+#         print(f"You entered: {user_input}")
+#     except KeyboardInterrupt:
+#         print("\nExiting...")
+#         break
