42d5a0a9cae59775feb094d7ca34f6af527f4ead
Anton <a.chertok@akatech.com.ua>
2025-01-07 09:40:19 +0000

    Update config.py

--- src/cli/config.py
+++ src/cli/config.py
@@ -2,16 +2,45 @@ from enum import Enum
 
 
 class Commands(Enum):
-    ADD = "add"
-    FIND = "find"
-    UPDATE = "update"
+    FIND_CONTACT = "find-contact"
+    ADD_CONTACT = "add-contact"
+    REMOVE_CONTACT = "remove-contact"
+    UPDATE_NAME = "update-name"
 
-    BIRTHDAY = "birthday"
-    ADD_BIRTHDAY = "add-birthday"
+    ADD_PHONE = "add-phone"
+    UPDATE_PHONE = "update-phone"
+    REMOVE_PHONE = "remove-phone"
 
-    ADDRESS = "address"
-    ADD_ADDRESS = "add-address"
+    ADD_ADDRESS = "address"
+    CLEAR_ADDRESS = "clear-address"
 
-    EMAIL = "email"
-    ADD_EMAIL = "add-email"
-    UPDATE_EMAIL = "update-email"
+    ADD_EMAIL = "email"
+    CLEAR_EMAIL = "clear-email"
+
+    ADD_BIRTHDAY = "birthday"
+    CLEAR_BIRTHDAY = "clear-birthday"
+
+    BIRTHDAYS = "birthdays"
+
+    ADD_NOTE = "add-note"
+    UPDATE_NOTE = "update-note"
+    REMOVE_NOTE = "remove-note"
+    ADD_TAGS = "add-tags"
+    REMOVE_TAGS = "remove-tags"
+    FIND_NOTES = "find-notes"
+    FIND_TAGS = "find-tags"
+
+#  birthday Name 12.13.2222
+#  email Name sdfsfd@asdd.cpm
+#
+#
+# add-note Title Text
+# update-note Title
+#
+#
+# add-tags Title askldl as;ldk asda;lsd asda sdasd
+#
+
+
+# @sure_remove
+# def remove_contact():
